1c5717f4cb82b7a212b130901936f53366d1fdfe
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-06 23:01:41 +0300

    Обычный класс Person

---
+++ classs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classs
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {        
+
+            Person person1 = new Person();
+            person1.PrintInfo("Андрей", 12);
+            Console.ReadLine();
+        }
+
+
+    }
+}
