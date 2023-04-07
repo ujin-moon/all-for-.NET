eeb0173b7c210dc36a8fb2a1abfedf5464be3e11
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-07 13:39:11 +0300

    Реализация структуры и класса

---
+++ clas/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using clas;
+
+Person person1 = new();
+person1.PrintInfo("Андрей", 12);
+Console.ReadLine();
--- classs/Program.cs
+++ classs/Program.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace classs
 {
-
     internal class Program
     {
+        public struct Dog {
+            public string name;
+            public int age;
+
+            public void Print()
+            {
+                Console.WriteLine($"Имя собаки: {name}, возвраст {age} лет");
+            }
+        
+        }
+
         static void Main(string[] args)
         {        
+            Dog dog1 = new Dog();
+            dog1.name = "Tom";
+            dog1.age = 5;
+            dog1.Print();
+
 
             Person person1 = new Person();
             person1.PrintInfo("Андрей", 12);
