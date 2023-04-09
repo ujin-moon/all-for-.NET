5f7da30f6a3c6f6e2d7a420eb56b0024211cf75e
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-10 02:12:43 +0300

    Операторы проверки на пустоту ?.

--- class/Program.cs
+++ class/Program.cs
@@ -4,31 +4,58 @@
 
 
 
+PrintNullable(5);       // 5
+PrintNullable(null);    // параметр равен null
 
-Person tom = new Person("Tom");
-Console.WriteLine(tom.name);
+PrintUpper("dsda");
+PrintUpper(null);
 
-//tom.name = "Sam"; // !Ошибка: нельзя изменить
+string? text = null;
+string name = text ?? "Tom";  // равно Tom, так как text равен null
+Console.WriteLine(name);    // Tom
 
+int? id = 200;
+int personid = id ?? 1; // равно 200, так как id не равен null
+Console.WriteLine(personid);    // 200
 
+Company company = new Company();
+company.WebSite = "Andersen.com";
+Person person = new Person();
+person.Company = company;
 
+PrintWebSite(person);
 
-
-
-
-string? name = null;
-
-PrintUpper(name);
-
-void PrintUpper(string text)
+void PrintNullable(int? number)
 {
-    if (text == null) Console.WriteLine("null");
-    else Console.WriteLine(text.ToUpper());
+    if (number.HasValue)//Проверка на то что имеет значение
+    {
+        Console.WriteLine(number.Value);
+        // аналогично
+        //Console.WriteLine(number);
+    }
+    else
+    {
+        Console.WriteLine("параметр равен null");
+    }
+}
+void PrintUpper(string? text)
+{
+    if (text is null) return;//is - используется как проверка и если верно, то = true, else = false (обратным будет is not)
+    Console.WriteLine(text.ToUpper());
+}
+void PrintWebSite(Person? person)
+{
+    Console.WriteLine(person?.Company?.WebSite?.ToUpper());
+}
+class Person
+{
+    public Company? Company { get; set; }   // место работы
+}
+class Company
+{
+    public string? WebSite { get; set; }    // веб-сайт компании
 }
 
 
 
-
-
-Console.ReadLine();
 
