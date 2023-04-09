da815fd9de3d208e6bfaa32d1486b8506c0805e2
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-09 23:28:57 +0300

    Создание перегруженных методов

---
+++ MyLlib/Calculator.cs
@@ -0,0 +1,28 @@
+﻿namespace MyLib
+{
+    public class Calculator
+    {
+        //Перегрузка методов за счет разного количества параметров, их типа, их порядка, их модификатора
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"Result is {result}");
+        }
+        public void Add(int a, int b, int c)
+        {
+            int result = a + b + c;
+            Console.WriteLine($"Result is {result}");
+        }
+        public int Add(int a, int b, int c, int d)
+        {
+            int result = a + b + c + d;
+            Console.WriteLine($"Result is {result}");
+            return result;
+        }
+        public void Add(double a, double b)
+        {
+            double result = a + b;
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
--- class/Program.cs
+++ class/Program.cs
@@ -1,43 +1,12 @@
 ﻿
+Calculator calc = new Calculator();
+calc.Add(4,5);//вызовет первый метод
+calc.Add(5,6,7);//вызовет второй метод
+calc.Add(6,7,8,9);//вызовет третий метод
+calc.Add(7.2,8.1);//вызовет четвертый метод
 
-using Persons;
 
-State state1 = new State(); // Структура State
-State state2 = new State();
-state2.x = 1;
-state2.y = 2;
-state1 = state2;
-state2.x = 5; // state1.x=1 по-прежнему
-Console.WriteLine(state1.x); // 1
-Console.WriteLine(state2.x); // 5
-
-Country country1 = new Country(); // Класс Country
-Country country2 = new Country();
-country2.x = 1;
-country2.y = 4;
-country1 = country2;
-country2.x = 7; // теперь и country1.x = 7, так как обе ссылки и country1 и country2 
-                // указывают на один объект в хипе
-Console.WriteLine(country1.x); // 7
-Console.WriteLine(country2.x); // 7
-Human human = new("Egor", "Frolov", 18);
-Console.WriteLine(human.Age);  // 1
-human.Age = 121;
-human.PrintInfo(human.Name, human.Age);
-
-Dogs bob = new("Bobs", 7);
-Console.WriteLine($"Кличка собаки: {bob.dog_name}, возраст: {bob.age_dog}");
 
 Console.ReadLine();
 
 
-struct State
-{
-    public int x;
-    public int y;
-}
-class Country
-{
-    public int x;
-    public int y;
-}
