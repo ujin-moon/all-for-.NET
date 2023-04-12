f325ff0adacb5ff73b12f673cf5d8060d21e57a3
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-12 13:16:38 +0300

    Создание обобщенных типо и универсальных переменных

--- MyLlib/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyLib
-{
-    public static class Operations
-    {
-        public static int Add(int x, int y) => x + y;
-        public static int Subtract(int x, int y) => x - y;
-        public static int Multiply(int x, int y) => x * y;
-    }
-}
--- class/Program.cs
+++ class/Program.cs
@@ -9,3 +9,14 @@ aircraft.Speed = 720;
 auto.Move();         // Легковая машина едет, со скоростью 70 км/ч
 ship.Move();        // Корабль плывет, со скоростью 80 км/ч
 aircraft.Move();    // Самолет летит, со скоростью 720 км/ч
+
+
+
+Person<int> tom = new Person<int>(546, "Tom");  // упаковка не нужна
+Person<string> bob = new Person<string>("abc123", "Bob");
+
+int tomId = tom.Id;      // распаковка не нужна
+string bobId = bob.Id;  // преобразование типов не нужно
+
+Console.WriteLine(tomId);   // 546
+Console.WriteLine(bobId);   // abc123
