241caf09ad4f5fd250af72947e0236a37ea1c4d1
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-12 13:51:53 +0300

    Использование ограничения методов и нескольких универсальных параметров

---
+++ MyLlib/Message.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console; // используем для сокращения 
+namespace MyLib
+{
+    public class Messenger<T, P>
+        where T : Message
+        where P : Person
+    {
+        public void SendMessage(P sender, P receiver, T message)
+        {
+            WriteLine($"Отправитель: {sender.Name}");
+            WriteLine($"Получатель: {receiver.Name}");
+            WriteLine($"Текст сообщения: {message.Text}");
+        }
+    }
+    public class Person
+    {
+        public string Name { get; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        
+    }
+    public class Message
+    {
+        public string Text { get; } // текст сообщения
+        public Message(string text)
+        {
+            Text = text;
+        }
+    }
+}
