be8a47a17ba4804a74d3ef27aeb052fba53eadbd
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-27 01:18:49 +0300

    Работа с индексаторами

--- class/Program.cs
+++ class/Program.cs
@@ -1,7 +1,67 @@
-﻿Messenger<Message, Person> google = new Messenger<Message, Person>();
+﻿Counter counter3 = new Counter { Value = 23 };
+Counter counter4 = new Counter { Value = 45 };
 
-Person tom = new("Tom");
-Person bob = new("Bob");
+bool result = counter3 > counter4;
+Counter c3 = counter3 + counter4;
+WriteLine(c3.Value);
+WriteLine(result);
 
-Message hello = new("Hello world");
-google.SendMessage(tom, bob, hello);
+
+
+var microsoft = new Company(new[]
+{
+    new Person("Tom"), new Person("Bob"), new Person("Sam"), new Person("Alice")
+});
+// получаем объект из индексатора
+Person firstPerson = microsoft[0];
+WriteLine(firstPerson.Name);  // Tom
+// переустанавливаем объект
+microsoft[0] = new Person("Mike");
+WriteLine(microsoft[0].Name);  // Mike
+
+WriteLine(firstPerson.Name);  // Tom
+
+Print();
+PrintValue("hello");
+
+string txt = """
+    if(i == love_you){
+    married == true;
+    }
+    else {
+    married == false;
+    }
+    """;
+
+WriteLine(txt); 
+
+void Print()
+{
+    string text = """
+              Привет
+              Мир
+              Как 
+              Дела?
+              """;
+    WriteLine(text);
+}
+
+void PrintValue(string val)
+{
+    string text = $"""
+              <element attr="content">
+                <body>
+                {val}
+                </body>
+              </element>
+              """;
+    //// или так 
+    //string text =  $$"""
+    //          <element attr="content">
+    //            
+    //            {{val}}
+    //            
+    //          </element>
+    //          """;
+    WriteLine(text);
+}
