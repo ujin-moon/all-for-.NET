5d8f62f2d4ecdce787245ba43ce23c5cf03b18f7
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-27 16:09:31 +0300

    Работа с датами и временем

--- class/Program.cs
+++ class/Program.cs
@@ -1,42 +1,25 @@
-﻿//Вывод многострочной строки
-using System.Text.RegularExpressions;
+﻿DateTime now = DateTime.Now;
 
-string txt = """ 
-    if(i == love_you){
-    married = true;
-    }
-    else{
-    married = false;
-    }
-    """;
-WriteLine(txt);
+WriteLine($"D: {now.ToString("D")}");
+WriteLine($"d: {now.ToString("d")}");
+WriteLine($"F: {now.ToString("F")}");
+WriteLine($"f: {now:f}");
+WriteLine($"G: {now:G}");
+WriteLine($"g: {now:g}");
+WriteLine($"M: {now:M}");
+WriteLine($"O: {now:O}");
+WriteLine($"o: {now:o}");
+WriteLine($"R: {now:R}");
+WriteLine($"s: {now:s}");
+WriteLine($"T: {now:T}");
+WriteLine($"t: {now:t}");
+WriteLine($"U: {now:U}");
+WriteLine($"u: {now:u}");
+WriteLine($"Y: {now:Y}");
+WriteLine(now.ToString("hh:mm:ss"));
 
-
-//Регулярные выражения
-string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
-var data = new string[]
-{
-    "tom@gmail.com",
-    "+12345678999",
-    "bob@yahoo.com",
-    "+13435465566",
-    "sam@yandex.ru",
-    "+43743989393"
-};
-
-WriteLine("Email List");
-for (int i = 0; i < data.Length; i++)
-{
-    if (Regex.IsMatch(data[i], pattern, RegexOptions.IgnoreCase))
-    {
-        WriteLine(data[i]);
-    }
-}
-
-string phoneNumber = "+1(876)-234-12-98";
-string pattern1 = @"\D";
-string target = "";
-Regex regex = new Regex(pattern1);
-string result = regex.Replace(phoneNumber, target);
-WriteLine(result);  // 18762341298
+now = now.AddDays(1);   // 28.04.2023
+now = now.AddMonths(4); // 28.08.2023
+now = now.AddYears(-1); // 28.08.2022
+WriteLine(now.ToShortDateString());  // 28.08.2022
+WriteLine(now.ToLongDateString());   // 28 августа 2022 г.WriteLine(now.ToString("dd.MM.yyyy"));
