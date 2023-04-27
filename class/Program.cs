fe0e22303c677cd2e17dc5d55ba8e7b488743813
ujin-moon <evgeniy.sedin@internet.ru>
2023-04-28 02:27:03 +0300

    Работа с датами и временем

--- class/Program.cs
+++ class/Program.cs
@@ -23,3 +23,9 @@ now = now.AddMonths(4); // 28.08.2023
 now = now.AddYears(-1); // 28.08.2022
 WriteLine(now.ToShortDateString());  // 28.08.2022
 WriteLine(now.ToLongDateString());   // 28 августа 2022 г.WriteLine(now.ToString("dd.MM.yyyy"));
+
+
+TimeOnly time = new TimeOnly(14, 23, 30);
+WriteLine(time.Hour);       // 14
+WriteLine(time.Minute);     // 23
+WriteLine(time.Second);     // 30
